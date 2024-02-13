d7a6afb5cbc7350e2859d6b5232d9da624fbcfe6
VladUs <vladustyugo@gmail.com>
2024-02-14 02:05:06 +0700

    feat: init project

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './styles/index.scss';
+import {Routes, Route, Link} from "react-router-dom";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {Suspense} from "react";
+import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classNames/classNames";
+
+const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <button onClick={toggleTheme}>THEME</button>
+            <Link to={'/'}>Main</Link>
+            <Link to={'/about'}>About</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                </Routes>
+            </Suspense>
+        </div>
+    );
+};
+
+export default App;
