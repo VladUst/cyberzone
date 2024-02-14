8414e9963ff7572c41b88c0049122879a7037e27
VladUs <vladustyugo@gmail.com>
2024-02-14 15:01:22 +0700

    refactor: compliance with the FSD methodology

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,7 +1,14 @@
 import {ResolveOptions} from "webpack";
+import {BuildOptions} from "./types/config";
 
-export function buildResolvers(): ResolveOptions {
+export function buildResolvers(options: BuildOptions): ResolveOptions {
     return {
         extensions: ['.tsx', '.ts', '.js'],
+        // чтобы webpack не ломался на абсолютгых импортах
+        preferAbsolute: true,
+        modules: [options.paths.src, 'node_modules'],
+        // для каждого модуля главным будет index файл (в нем public API)
+        mainFiles: ['index'],
+        alias: {}
     }
 }
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './styles/index.scss';
 import {Routes, Route, Link} from "react-router-dom";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {Suspense} from "react";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider/lib/useTheme";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
@@ -17,8 +17,8 @@ const App = () => {
             <Link to={'/about'}>About</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync/>}/>
-                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/'} element={<MainPage/>}/>
                 </Routes>
             </Suspense>
         </div>
