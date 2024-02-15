a5caf5f48806884d43ec903bcfbcf48907d6b8b4
VladUs <vladustyugo@gmail.com>
2024-02-15 16:21:41 +0700

    feat: add navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
 import './styles/index.scss';
-import {Routes, Route, Link} from "react-router-dom";
-import {Suspense} from "react";
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider/lib/useTheme";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import AppRouter from "app/providers/router/ui/AppRouter";
+import {Navbar} from "widgets/Navbar";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
+            <Navbar/>
+            <AppRouter/>
             <button onClick={toggleTheme}>THEME</button>
-            <Link to={'/'}>Main</Link>
-            <Link to={'/about'}>About</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage/>}/>
-                    <Route path={'/'} element={<MainPage/>}/>
-                </Routes>
-            </Suspense>
         </div>
     );
 };
