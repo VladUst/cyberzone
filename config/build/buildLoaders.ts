6bd5009cad7c95ea4ecdb92969855d2da60bb10d
VladUs <vladustyugo@gmail.com>
2024-02-21 01:20:18 +0700

    feat: add theme switcher + button component

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,6 +3,11 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {BuildOptions} from "./types/config";
 
 export function buildLoaders({isDev}: BuildOptions): RuleSetRule[] {
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    };
+
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
@@ -29,7 +34,19 @@ export function buildLoaders({isDev}: BuildOptions): RuleSetRule[] {
         use: 'ts-loader',
         exclude: /node_modules/,
     };
+
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    };
+
     return  [
+        fileLoader,
+        svgLoader,
         typescriptLoader,
         cssLoader
     ]
--- src/app/providers/ThemeProvider/index.ts
+++ src/app/providers/ThemeProvider/index.ts
@@ -1,2 +1,4 @@
 import ThemeProvider from "./ui/ThemeProvider";
-export {ThemeProvider};
+import {useTheme} from "./lib/useTheme";
+import {Theme} from "./lib/ThemeContext";
+export {ThemeProvider, useTheme, Theme};
