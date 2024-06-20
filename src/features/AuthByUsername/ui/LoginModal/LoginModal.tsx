d031e55faa20a4508ab807f5ddcf2ae8db579a1e
VladUs <vladustyugo@gmail.com>
2024-06-20 23:08:06 +0700

    build: add husky precommit

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -2,7 +2,6 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { Modal } from 'shared/ui/Modal/Modal';
 import { LoginForm } from '../LoginForm/LoginForm';
-import cls from './LoginModal.module.scss';
 
 interface LoginModalProps {
     className?: string
@@ -14,7 +13,7 @@ export const LoginModal = ({ className, isOpen, onClose }: LoginModalProps) => {
     const { t } = useTranslation();
     return (
         <Modal
-            className={classNames(cls.LoginModal, {}, [className])}
+            className={classNames('', {}, [className])}
             isOpen={isOpen}
             onClose={onClose}
             lazy
