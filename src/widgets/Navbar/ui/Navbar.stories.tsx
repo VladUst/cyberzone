2ad27f60561d6a6a6a61afb70f528305c0992520
VladUs <vladustyugo@gmail.com>
2024-06-26 23:17:33 +0700

    test: test auth slices and loginByUsername async thunk

--- src/widgets/Navbar/ui/Navbar.stories.tsx
+++ src/widgets/Navbar/ui/Navbar.stories.tsx
@@ -24,9 +24,3 @@ Light.decorators = [StoreDecorator({
 export const Dark = Template.bind({});
 Dark.args = {};
 Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({})];
-
-export const AuthNavbar = Template.bind({});
-AuthNavbar.args = {};
-AuthNavbar.decorators = [StoreDecorator({
-    user: { authData: {} },
-})];
