6ce15891a3c5f6077d21258db6c20d0ff9f892fb
VladUs <vladustyugo@gmail.com>
2024-09-06 23:44:35 +0700

    perf: add supported browsers

--- src/features/NotoficationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/features/NotoficationButton/ui/NotificationButton/NotificationButton.tsx
@@ -7,7 +7,6 @@ import { NotificationList } from '@/entities/Notification';
 import { Popover } from '@/shared/ui/Popups';
 import { Drawer } from '@/shared/ui/Drawer/Drawer';
 import { useDeviceDetect } from '@/shared/lib/hooks/useDeviceDetect';
-import { AnimationProvider } from '@/shared/lib/components/AnimationProvider';
 import cls from './NotificationButton.module.scss';
 
 interface NotificationButtonProps {
@@ -38,11 +37,9 @@ export const NotificationButton = memo((props: NotificationButtonProps) => {
         return (
             <>
                 {trigger}
-                <AnimationProvider>
-                    <Drawer isOpen={isOpen} onClose={onClose}>
-                        <NotificationList />
-                    </Drawer>
-                </AnimationProvider>
+                <Drawer isOpen={isOpen} onClose={onClose}>
+                    <NotificationList />
+                </Drawer>
             </>
         );
     }
