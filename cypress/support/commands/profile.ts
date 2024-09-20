d57065f60dde13c39ee57052d3e64a912352bdc7
VladUs <vladustyugo@gmail.com>
2024-09-21 00:24:02 +0700

    test: e2e tests for profile and articles

---
+++ cypress/support/commands/profile.ts
@@ -0,0 +1,34 @@
+export const updateProfile = (firstname: string, lastname: string) => {
+    cy.getByTestId('EditableProfileCardHeader.EditButton').click();
+    cy.getByTestId('ProfileCard.firstname').clear().type(firstname);
+    cy.getByTestId('ProfileCard.lastname').clear().type(lastname);
+    cy.getByTestId('EditableProfileCardHeader.SaveButton').click();
+};
+
+export const resetProfile = (profileId: string) => {
+    return cy.request({
+        method: 'PUT',
+        url: `http://localhost:8000/profile/${profileId}`,
+        headers: { Authorization: 'asasf' },
+        body: {
+            id: profileId,
+            first: 'test',
+            lastname: 'user',
+            age: 25,
+            currency: 'USD',
+            country: 'Russia',
+            city: 'New York',
+            username: 'testuser',
+            avatar: 'https://pic.rutubelist.ru/user/3b/27/3b2758ad5492a76b578f7ee072e4e894.jpg',
+        },
+    });
+};
+
+declare global {
+    namespace Cypress {
+        interface Chainable {
+            updateProfile(firstname: string, lastname: string): Chainable<void>;
+            resetProfile(profileId: string): Chainable<void>;
+        }
+    }
+}
--- src/entities/Rating/ui/RatingCard/RatingCard.tsx
+++ src/entities/Rating/ui/RatingCard/RatingCard.tsx
@@ -61,6 +61,7 @@ export const RatingCard = memo((props: RatingCardProps) => {
                 title={feedbackTitle}
             />
             <Input
+                data-testid="RatingCard.Input"
                 value={feedback}
                 onChange={setFeedback}
                 placeholder={t('Ваш отзыв')}
@@ -69,7 +70,11 @@ export const RatingCard = memo((props: RatingCardProps) => {
     );
 
     return (
-        <Card max className={className}>
+        <Card
+            data-testid="RatingCard"
+            max
+            className={className}
+        >
             <VStack align="center" gap="8">
                 <Text title={starsCount ? t('Спасибо за оценку!') : title} />
                 <StarRating selectedStars={starsCount} size={40} onSelect={onSelectStars} />
@@ -79,10 +84,17 @@ export const RatingCard = memo((props: RatingCardProps) => {
                     <VStack max gap="32">
                         {modalContent}
                         <HStack max gap="16" justify="end">
-                            <Button onClick={cancelHandle} theme={ButtonTheme.OUTLINE_RED}>
+                            <Button
+                                data-testid="RatingCard.Close"
+                                onClick={cancelHandle}
+                                theme={ButtonTheme.OUTLINE_RED}
+                            >
                                 {t('Закрыть')}
                             </Button>
-                            <Button onClick={acceptHandle}>
+                            <Button
+                                data-testid="RatingCard.Send"
+                                onClick={acceptHandle}
+                            >
                                 {t('Отправить')}
                             </Button>
                         </HStack>
